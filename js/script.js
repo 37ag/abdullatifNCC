dc41c67fe2e81754111b8dbda0c499b33d10349d
37ag <134615297+37ag@users.noreply.github.com>
2023-05-29 03:09:03 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let searchForm = document.querySelector('.search-form-container');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.shopping-cart-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    navbar.classList.remove('active');
+}
+
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next(){
+   slides[index].classList.remove('active');
+   index = (index + 1) % slides.length;
+   slides[index].classList.add('active');
+}
+
+function prev(){
+   slides[index].classList.remove('active');
+   index = (index - 1 + slides.length) % slides.length;
+   slides[index].classList.add('active');
+}
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+   acco.onclick = () =>{
+      accordion.forEach(remove => remove.classList.remove('active'));
+      acco.classList.add('active');
+   }
+});
